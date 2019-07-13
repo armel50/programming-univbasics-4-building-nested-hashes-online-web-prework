ade5a19b1bba860568bcf805d0d0ee8101bf5e03
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-13 06:00:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,8 @@ def monopoly_with_third_tier
  monopoly[:railroads][:names] = {
    :reading_railroad => {},
    :pennsylvania_railroad => {},
-   :b_and_o_railroad => {}
+   :b_and_o_railroad => {},
+   :shortline => {}
  }
   monopoly[:railroads][:rent_in_dollars] = {:one_piece_owned => 25,
     :two_pieces_owned => 50,
