c47e7ea00419532b65576f87698c424ac6931bd9
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-13 06:07:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_fourth_tier
  monopoly[:railroads][:pieces] = 4
  monopoly[:railroads][:names] = {
    :reading_railroad => {
-     :mortgage_value => :$100
+     "mortgage_value" => "$100"
    },
    :pennsylvania_railroad => {},
    :b_and_o_railroad => {},
