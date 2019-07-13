70c48c80b1db3f1a3cfb329ec907310d86f94c27
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-13 06:09:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,7 +41,9 @@ def monopoly_with_fourth_tier
    :b_and_o_railroad => {
      "mortgage_value" => "$400"
    },
-   :shortline => {}
+   :shortline => {
+     "mortgage_value" => "$800"
+   }
  }
   monopoly[:railroads][:rent_in_dollars] = {:one_piece_owned => 25,
     :two_pieces_owned => 50,
